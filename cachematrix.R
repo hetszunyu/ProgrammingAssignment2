522cf2bd7949152413a3f2d39365e30e3c5019d1
hetszunyu <37790505+hetszunyu@users.noreply.github.com>
2018-04-17 16:51:22 +0200

    Updated with comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,7 @@
 ## slight modification of the original vector example in the assignment
 ##description.
 
-
+## For key takeaway from this homework, see comments in code
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
@@ -10,12 +10,14 @@ makeCacheMatrix <- function(x = matrix()) {
                 m <<- NULL  ##sets m also in parent environment (note to self)
         }
         get <- function() x
-        setinv <- function(inv) m <<- inv
+        setinv <- function(inv) m <<- inv ## these <<-s ensure that m is set to inv outside the function
         getinv <- function() m
         list(set = set, get = get, setinv = setinv,
              getinv = getinv)
 }
 
+## cacheSolve is easier to understand, it basically check if there's a cached version available and then
+## proceeds to calculate the inverse if there's none (and retreives the cached inverse if there is)
 cacheSolve <- function(x, ...) {
        m <- x$getinv()
         if(!is.null(m)) {
